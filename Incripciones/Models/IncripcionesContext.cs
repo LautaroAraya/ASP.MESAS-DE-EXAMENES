0cf14af81c94169b252aefa6805a39619b520568
lautyaraya <lautyaraya65@gmail.com>
2024-06-06 14:36:46 -0300

    nuevos cambios

---
+++ Incripciones/Models/IncripcionesContext.cs
@@ -0,0 +1,18 @@
+﻿using Incripciones.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inscripcioness.Models
+{
+    public class InscripcionesContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;
+                           Database=InscripcionesContext;
+                           User Id = sa; Password = 123;
+                           Encrypt=false");
+        }
+       public virtual DbSet<Alumno> Alumnos { get; set; }
+    }
+}
